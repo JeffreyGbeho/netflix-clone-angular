56cb81d3de6b9b6f0ceaa961b25db1370195860c
unknown <jeff.gbeho@gmail.com>
2024-02-29 01:19:13 +0100

    redirect auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,9 +1,11 @@
-import { Inject } from '@angular/core';
+import { Inject, inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
   const token = localStorage.getItem('JWT_TOKEN');
   if (!token) {
+    router.navigate(['/login']);
     return false;
   }
   return true;
