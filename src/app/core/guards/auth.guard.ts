42b22b3d175face6e43286fdb0c20f21e38be297
unknown <jeff.gbeho@gmail.com>
2024-03-06 23:55:35 +0100

    patch authGuard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,12 +1,17 @@
 import { Inject, inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router)
-  const token = localStorage.getItem('JWT_TOKEN');
-  if (!token) {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  if (!token || !authService.isTokenValid(token)) {
+    localStorage.removeItem('JWT_TOKEN');
     router.navigate(['/login']);
     return false;
   }
+
   return true;
 };
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,20 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
+
+  public getToken(): string | null {
+    return localStorage.getItem('JWT_TOKEN');
+  }
+
+  isTokenValid(token: string): boolean {
+        try {
+            const decodedToken: any = jwtDecode(token);
+            const currentTime = Date.now() / 1000;
+            return decodedToken.exp > currentTime;
+        } catch (error) {
+            return false;
+        }
+    }
 
   public login(email: string, password: string) {
     return this.http.post('http://localhost:8080/api/auth/login', { email, password });
