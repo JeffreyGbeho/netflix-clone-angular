22fb2d9dba1e60440d85e74822f458d9a30edbc9
unknown <jeff.gbeho@gmail.com>
2024-03-17 11:25:51 +0100

    add list of movies

--- src/app/pages/browse-movies/browse-movies.component.ts
+++ src/app/pages/browse-movies/browse-movies.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../common/header/header.component';
 import { MoviesService } from '../../shared/services/movies.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-browse-movies',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule, RouterLink],
   templateUrl: './browse-movies.component.html',
   styleUrl: './browse-movies.component.scss',
 })
 export class BrowseMoviesComponent {
   public movies: any;
+  public movieTrending: any;
   public videoData: any;
 
   constructor(private movieService: MoviesService) {
     this.movieService.getMovies().subscribe((response) => {
       console.log(response);
       this.movies = response;
+      this.movieTrending = this.movies[0];
       this.getMoviesByTitle('Spring');
     });
+  }
+
+  public mutedTrendingMovie(): void {
+    const video = document.getElementById('videoTrending') as HTMLVideoElement;
+    video.muted = !video.muted;
   }
 
   private getMoviesByTitle(title: string): void {
